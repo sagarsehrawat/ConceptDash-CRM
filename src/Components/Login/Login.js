e4a363ce3d26668155f7c0b0c990a1abd8ff236f
daemon25 <lakshayrose0000@gmail.com>
2023-02-16 22:50:50 +0530

    Privileges

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState,useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import AuthenticationContext from '../../Context/AuthContext';
 import './Login.css'
@@ -11,7 +11,7 @@ const Login = () => {
   const { privileges, setPrivileges } = useContext(AuthContext)
   useEffect(() => {
     const department = localStorage.getItem('department')
-    if(department) {
+    if (department) {
       switch (department) {
         case 'Admin':
           navigate('/admin')
@@ -25,65 +25,69 @@ const Login = () => {
         default:
           break;
       }
-  }
-  
-    
+    }
+
+
   }, [])
-  
-  
-  const context = useContext(AuthenticationContext)
-  const {user, setUser} = context;
+
+
   const [username, setusername] = useState('')
   const [password, setpassword] = useState('')
-  
-  const onLogin = (e) =>{
+
+  const onLogin = async (e) => {
     e.preventDefault()
-    handleSubmit(e);
-    Call();
+   handleSubmit(e);
   }
-  const Call = async () =>{
-    await axios
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post(HOST + LOGIN, { 'username': username, 'password': password }).then((res) => {
+      if (res.data.error === "Email or Password is Incorrect") {
+        alert('Incorrect Username or Password');
+      } else {
+        if (!res.data.success)
+          alert('Something Went Wrong...')
+      }
+      localStorage.setItem('auth', res.data.auth)
+      localStorage.setItem('department', res.data.user.department)
+      localStorage.setItem('emailWork', res.data.user.emailWork)
+      localStorage.setItem('employeeId', res.data.user.employeeId)
+      localStorage.setItem('employeeName', res.data.user.employeeName)
+
+      axios
         .get(HOST + GET_EMPLOYEE_PRIVILEGES, {
           headers: { auth: "Rose " + localStorage.getItem("auth"), employeeid: localStorage.getItem('employeeId') },
         })
         .then((res) => {
-          setPrivileges(res.data.res);
+          let arr = []
+          res.data.res.map(e => {
+            arr.push(e.Privilege)
+          })
+          setPrivileges(arr);
+
+          switch (localStorage.getItem("department")) {
+            case 'Admin':
+              navigate('/admin')
+              break;
+            case 'Engineer':
+              navigate('/engineers')
+              break;
+            case 'Manager':
+              navigate('/manager')
+              break;
+            default:
+              break;
+          }
         })
         .catch((err) => {
           console.log(err);
         });
-  }
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post(HOST + LOGIN,{'username':username,'password':password}).then((res) => {
-      if(res.data.error==="Email or Password is Incorrect") {
-          alert('Incorrect Username or Password');
-      } else {
-        if(!res.data.success)
-          alert('Something Went Wrong...')
-      }
-            localStorage.setItem('auth',res.data.auth)
-            localStorage.setItem('department',res.data.user.department)
-            localStorage.setItem('emailWork',res.data.user.emailWork)
-            localStorage.setItem('employeeId',res.data.user.employeeId)
-            localStorage.setItem('employeeName', res.data.user.employeeName)
-          
-            switch (res.data.user.department) {
-              case 'Admin':
-                navigate('/admin')
-                break;
-              case 'Engineer':
-                navigate('/engineers')
-                break;
-              case 'Manager':
-                navigate('/manager')
-                break;
-              default:
-                break;
-            }}
-          ).catch((err) => {
-            console.log(err)
-          })
+
+      
+    }
+    ).catch((err) => {
+      console.log(err)
+    })
   }
   return (
     <>
@@ -94,10 +98,10 @@ const Login = () => {
           </div>
           <form>
             <div className=" mb-4">
-              <input type="text" className="form-control shadow-none input-home" id="inputEmail3" placeholder='Username' onChange={(e)=>{setusername(e.target.value)}} />
+              <input type="text" className="form-control shadow-none input-home" id="inputEmail3" placeholder='Username' onChange={(e) => { setusername(e.target.value) }} />
             </div>
             <div className=" mb-3">
-              <input type="password" className="form-control shadow-none input-home" id="password" placeholder='Password' style={{ marginBottom : "4rem"}} onChange={(e)=>{setpassword(e.target.value)}}/>
+              <input type="password" className="form-control shadow-none input-home" id="password" placeholder='Password' style={{ marginBottom: "4rem" }} onChange={(e) => { setpassword(e.target.value) }} />
             </div>
             <div className='d-flex flex-row justify-content-center my-1'>
               <button type="submit" className="btn btn-home" onClick={onLogin}>Login</button>
