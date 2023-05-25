8bfa59c2e54063ad1ae392a8d15b06ca0183b64b
Sagar Sehrawat <sagar02sehrawat@gmail.com>
2023-05-25 19:50:09 +0530

    commit

--- src/Components/v2/Announcements.js
+++ src/Components/v2/Announcements.js
@@ -3,6 +3,11 @@ import InputEmoji from 'react-input-emoji'
 import './Announcements.css'
 import url from '../../Images/url.svg'
 import attachment from '../../Images/attachment.svg'
+import ellipse from '../../Images/Ellipse.png'
+import dots from '../../Images/3dots.svg'
+import bg from '../../Images/annImage.png'
+import like from '../../Images/like.svg'
+import comment from '../../Images/comment.svg'
 
 
 function Announcements(props) {
@@ -21,7 +26,7 @@ function Announcements(props) {
     seturlText(e.target.value)
   }
   return (
-    <div>
+    <div style={{display:'flex', flexDirection:'row'}}>
       <div className='left-area'>
         <div className='formHeading'>Make New Announcement</div>
         <div className='formLabel-1'>Title<span style={{color:'red'}}>*</span></div>
@@ -68,6 +73,66 @@ function Announcements(props) {
             onChange={handleURLChange}
           />
         </div>
+        <button className='announceBtn'><div className='announceText'>Announce</div></button>
+      </div>
+      <div className='right-area'>
+          <div className='rightHeading'>Announcements</div>
+          <div className='announcementCont'>
+            <div className='upperHalf'>
+              <div className='details' style={{display:'flex', flexDirection:'row'}}>
+                <img src={ellipse} height={48} width={48}/>
+                <div style={{flexDirection:'column', marginLeft:'8px'}}>
+                  <div className='annName'>Sagar Sehrawat</div>
+                  <div className='annDesignation'>Software Engineer</div>
+                  <div className='annTime'>10 m ago</div>
+                </div>
+                <div className='annAbout'><img src={dots} /></div>
+              </div>
+              <div className='annHeading'>Taskforce Beta Release 🥂</div>
+              <div className='annBody'>The wait is over!! The Beta version of Taskforce is scheduled to happen on 25th of May, 2023.
+                We are eagerly waiting for the release and waiting for your response! 🤞🏻
+              </div>
+            </div>
+            <div className='imaging'><img width='100%' src={bg} /></div>
+            <div className='lowerHalf'>
+                <div style={{display:'flex', flexDirection:'row'}}>
+                  <img src={like} />
+                  <div className='likeText'>Like</div>
+                </div>
+                <div style={{display:'flex', flexDirection:'row', marginLeft:'3em'}}>
+                  <img src={comment} />
+                  <div className='likeText'>Comment</div>
+                </div>
+            </div>
+          </div>
+          <div className='announcementCont'>
+            <div className='upperHalf'>
+              <div className='details' style={{display:'flex', flexDirection:'row'}}>
+                <img src={ellipse} height={48} width={48}/>
+                <div style={{flexDirection:'column', marginLeft:'8px'}}>
+                  <div className='annName'>Sagar Sehrawat</div>
+                  <div className='annDesignation'>Software Engineer</div>
+                  <div className='annTime'>10 m ago</div>
+                </div>
+                <div className='annAbout'><img src={dots} /></div>
+              </div>
+              <div className='annHeading'>Taskforce Beta Release 🥂</div>
+              <div className='annBody'>The wait is over!! The Beta version of Taskforce is scheduled to happen on 25th of May, 2023.
+                We are eagerly waiting for the release and waiting for your response! 🤞🏻
+              </div>
+            </div>
+            <div className='imaging'><img width='100%' src={bg} /></div>
+            <div className='lowerHalf'>
+                <div style={{display:'flex', flexDirection:'row'}}>
+                  <img src={like} />
+                  <div className='likeText'>Like</div>
+                </div>
+                <div style={{display:'flex', flexDirection:'row', marginLeft:'3em'}}>
+                  <img src={comment} />
+                  <div className='likeText'>Comment</div>
+                </div>
+            </div>
+          </div>
       </div>
     </div>
   )
