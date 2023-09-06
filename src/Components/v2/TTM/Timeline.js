d7b4f94202b1e8b20d930963cd96401dd6f28ff8
Sagar Sehrawat <sagar02sehrawat@gmail.com>
2023-09-06 15:13:32 +0530

    push

--- src/Components/v2/TTM/Timeline.js
+++ src/Components/v2/TTM/Timeline.js
@@ -1,108 +1,102 @@
 import React, { useEffect, useState } from 'react'
 import './Timeline.css'
 import { GanttComponent, EditDialogFieldsDirective, DayMarkers, EditDialogFieldDirective, Inject, Edit, Selection, Toolbar, ColumnsDirective, ColumnDirective, EventMarkersDirective, EventMarkerDirective } from '@syncfusion/ej2-react-gantt';
-import { editingResources } from './Data';
+import { editingData, editingResources } from './Data';
 import { GET_TTM, HOST } from '../../Constants/Constants';
 import LoadingSpinner from '../../Loader/Loader';
 import axios from 'axios';
-// import { editingData } from './Data'
 function Timeline() {
 
-  const [editingData, seteditingData] = useState([])
+//   const [editingData, seteditingData] = useState([])
   const [isLoading, setisLoading] = useState(false)
-    useEffect(() => {
-        setisLoading(true);
-        const call = async() => {
-          await axios
-            .get(HOST + GET_TTM, {
-              headers: { auth: "Rose " + localStorage.getItem("auth"), proposalId:"78" }
-            })
-            .then((res) => {
-              let data = JSON.parse(res.data.res[0].Data)
-              seteditingData(data)
-              // seteditingData(JSON.parse(res.data.res[0].Data))
-            })
-            .catch((err) => {
-              console.log(err);
-            });
+//     useEffect(() => {
+//         setisLoading(true);
+//         const call = async() => {
+//           await axios
+//             .get(HOST + GET_TTM, {
+//               headers: { auth: "Rose " + localStorage.getItem("auth"), proposalId:"78" }
+//             })
+//             .then((res) => {
+//               let data = JSON.parse(res.data.res[0].Data)
+//               seteditingData(data)
+//               // seteditingData(JSON.parse(res.data.res[0].Data))
+//             })
+//             .catch((err) => {
+//               console.log(err);
+//             });
     
-          setisLoading(false);
-        };
-        call();
-      }, []);
+//           setisLoading(false);
+//         };
+//         call();
+//       }, []);
     
-    const taskFields = {
-        id: 'TaskID',
-        name: 'TaskName',
-        startDate: 'StartDate',
-        endDate: 'EndDate',
-        duration: 'Duration',
-        progress: 'Progress',
-        dependency: 'Predecessor',
-        child: 'subtasks',
-        notes: 'info',
-        resourceInfo: 'resources'
-    };
-    const resourceFields = {
-        id: 'resourceId',
-        name: 'resourceName'
-    };
-    const editSettings = {
-        allowAdding: true,
-        allowEditing: true,
-        allowDeleting: true,
-        allowTaskbarEditing: true,
-        showDeleteConfirmDialog: true,
-        
-    };
-    const splitterSettings = {
-        columnIndex: 2
-    };
-    const gridLines = 'Both';
-    const toolbar = ['Add', 'Edit', 'Update', 'Delete', 'Cancel', 'ExpandAll', 'CollapseAll', 'Indent', 'Outdent'];
-    const timelineSettings = {
-        topTier: {
-            unit: 'Week',
-            format: 'MMM dd, y',
-        },
-        bottomTier: {
-            unit: 'Day',
-        },
-    };
-    const labelSettings = {
-        leftLabel: 'TaskName',
-        rightLabel: 'resources'
-    };
+const taskFields = {
+    id: 'TaskID',
+    name: 'TaskName',
+    startDate: 'StartDate',
+    endDate: 'EndDate',
+    duration: 'Duration',
+    progress: 'Progress',
+    dependency: 'Predecessor',
+    child: 'subtasks',
+    notes: 'info',
+    resourceInfo: 'resources'
+};
+const resourceFields = {
+    id: 'resourceId',
+    name: 'resourceName'
+};
+const editSettings = {
+    allowAdding: true,
+    allowEditing: true,
+    allowDeleting: true,
+    allowTaskbarEditing: true,
+    showDeleteConfirmDialog: true
+};
+const splitterSettings = {
+    columnIndex: 2
+};
+const projectStartDate = new Date('07/25/2023');
+const projectEndDate = new Date('09/28/2023');
+const gridLines = 'Both';
+const toolbar = ['Add', 'Edit', 'Update', 'Delete', 'Cancel', 'ExpandAll', 'CollapseAll', 'Indent', 'Outdent'];
+const timelineSettings = {
+    topTier: {
+        unit: 'Week',
+        format: 'MMM dd, y',
+    },
+    bottomTier: {
+        unit: 'Day',
+    },
+};
+const labelSettings = {
+    leftLabel: 'TaskName',
+    rightLabel: 'resources'
+};
   return (
    isLoading?<LoadingSpinner/>:
-    <div className='control-pane'>
-      <div className='control-section'>
-        <GanttComponent id='Editing' dataSource={editingData} dateFormat={'MMM dd, y'} treeColumnIndex={1} allowSelection={true} showColumnMenu={false} highlightWeekends={true} allowUnscheduledTasks={true} taskFields={taskFields} timelineSettings={timelineSettings} labelSettings={labelSettings} splitterSettings={splitterSettings} height='410px' editSettings={editSettings} gridLines={gridLines} toolbar={toolbar} resourceFields={resourceFields} resources={editingResources}>
-          <ColumnsDirective>
-            <ColumnDirective field='TaskID' width='80'></ColumnDirective>
-            <ColumnDirective field='TaskName' headerText='Job Name' width='250' clipMode='EllipsisWithTooltip'></ColumnDirective>
-            <ColumnDirective field='StartDate'></ColumnDirective>
-            <ColumnDirective field='Duration'></ColumnDirective>
-            <ColumnDirective field='Progress'></ColumnDirective>
-            <ColumnDirective field='Predecessor'></ColumnDirective>
-          </ColumnsDirective>
-          <EditDialogFieldsDirective>
-            <EditDialogFieldDirective type='General' headerText='General'></EditDialogFieldDirective>
-            <EditDialogFieldDirective type='Dependency'></EditDialogFieldDirective>
-            <EditDialogFieldDirective type='Resources'></EditDialogFieldDirective>
-            <EditDialogFieldDirective type='Notes'></EditDialogFieldDirective>
-          </EditDialogFieldsDirective>
-          {/* <EventMarkersDirective>
-            <EventMarkerDirective day={eventMarkerDay1} label='Project approval and kick-off'></EventMarkerDirective>
-            <EventMarkerDirective day={eventMarkerDay2} label='Foundation inspection'></EventMarkerDirective>
-            <EventMarkerDirective day={eventMarkerDay3} label='Site manager inspection'></EventMarkerDirective>
-            <EventMarkerDirective day={eventMarkerDay4} label='Property handover and sign-off'></EventMarkerDirective>
-          </EventMarkersDirective> */}
-          <Inject services={[Edit, Selection, Toolbar, DayMarkers]}/>
-        </GanttComponent>
-      </div>
+   <div className='control-pane'>
+   <div className='control-section'>
+     <GanttComponent id='Editing' dataSource={editingData} dateFormat={'MMM dd, y'} treeColumnIndex={1} allowSelection={true} showColumnMenu={false} highlightWeekends={true} allowUnscheduledTasks={true} taskFields={taskFields} timelineSettings={timelineSettings} labelSettings={labelSettings} splitterSettings={splitterSettings} height='100%' editSettings={editSettings} toolbar={toolbar} resources={editingResources}>
+       <ColumnsDirective>
+         <ColumnDirective field='TaskID' width='80'></ColumnDirective>
+         <ColumnDirective field='TaskName' headerText='Milestones and Tasks' width='250' clipMode='EllipsisWithTooltip'></ColumnDirective>
+         <ColumnDirective field='StartDate'></ColumnDirective>
+         <ColumnDirective field='Duration'></ColumnDirective>
+         <ColumnDirective field='Progress'></ColumnDirective>
+         <ColumnDirective field='Predecessor'></ColumnDirective>
+       </ColumnsDirective>
+       <EditDialogFieldsDirective>
+         <EditDialogFieldDirective type='General' headerText='General'></EditDialogFieldDirective>
+         <EditDialogFieldDirective type='Dependency'></EditDialogFieldDirective>
+         <EditDialogFieldDirective type='Resources'></EditDialogFieldDirective>
+         <EditDialogFieldDirective type='Notes'></EditDialogFieldDirective>
+       </EditDialogFieldsDirective>
+       <Inject services={[Edit, Selection, Toolbar, DayMarkers]}/>
+     </GanttComponent>
+   </div>
 
-    </div>
+ </div>
   )
 }
 
