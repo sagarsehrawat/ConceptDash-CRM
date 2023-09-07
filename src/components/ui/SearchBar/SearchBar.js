715862fccbe8e2064812c234e7ed78b8ae807ff0
Lakshay Rose <lakshayrose0000@gmail.com>
2023-09-07 18:10:19 +0530

    Added Search Icon

--- src/components/ui/SearchBar/SearchBar.js
+++ src/components/ui/SearchBar/SearchBar.js
@@ -1,48 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import './SearchBar.css'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "./SearchBar.css";
+import MagnifyingGlass from '../../../assets/icons/Magnifying_Glass.svg'
 
 const SearchBar = (props) => {
-    const {placeholder, searchFunc, apiFunc, style} = props;
-    const [search, setSearch] = searchFunc
-    const [api, setApi] = apiFunc;
+  const { placeholder, searchFunc, apiFunc, style } = props;
+  const [search, setSearch] = searchFunc;
+  const [api, setApi] = apiFunc;
 
-    useEffect(() => {
-        const delayDebounceFn = setTimeout(() => {
-        if(api!=null) setApi(api+1)
-        }, 500)
-    
-        return () => clearTimeout(delayDebounceFn)
-      }, [search])
+  const [isFocused, setIsFocused] = useState(false);
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearch(e.target.value)
-    }
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (api != null) setApi(api + 1);
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
 
   return (
-    <input
+    <div className="searchbar-container">
+        <img className={`searchbar-icon ${isFocused ? 'searchbar-icon-focus' : ''}`} src={MagnifyingGlass} alt="Search Icon"/>
+      <input
         className={`searchbar`}
         style={style}
         type="text"
         value={search}
         onChange={handleChange}
         placeholder={`Search ${placeholder}`}
-    />
-  )
-}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+    </div>
+  );
+};
 
 SearchBar.propTypes = {
-    placeholder: PropTypes.string,
-    searchFunc: PropTypes.array.isRequired,
-    apiFunc: PropTypes.array,
-    style: PropTypes.object
-}
+  placeholder: PropTypes.string,
+  searchFunc: PropTypes.array.isRequired,
+  apiFunc: PropTypes.array,
+  style: PropTypes.object,
+};
 
 SearchBar.defaultProps = {
-    placeholder: '',
-    apiFunc: [null, null],
-    style: {}
-}
+  placeholder: "",
+  apiFunc: [null, null],
+  style: {},
+};
 
-export default SearchBar
+export default SearchBar;
