8df9b2b8261f578457bde8b87f59cc84198541a2
Lakshay-prog2503 <79397379+Daemon-25@users.noreply.github.com>
2023-10-14 01:22:59 +0530

    Typeahead COmpleted

--- src/components/form/TFTypeahead/TFTypeahead.js
+++ src/components/form/TFTypeahead/TFTypeahead.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react'
 import PropTypes from "prop-types";
 import "./TFTypeahead.css";
 
-const TFTypeahead = ({ name, placeholder, value, onChange, options }) => {
+const TFTypeahead = ({ name, placeholder, value, onChange, options, required, readOnly }) => {
   const [isVisible, setisVisible] = useState(false);
+  // TODO : Implement Asynchronous Functionality
+  // TODO : Implement MultiSelect Functionality
+  console.log(value)
   return (
     <>
       <div className='typeahead-wrapper'>
@@ -15,20 +18,32 @@ const TFTypeahead = ({ name, placeholder, value, onChange, options }) => {
           className='typeahead-input'
           onFocus={(e) => setisVisible(true)}
           onBlur={(e) => setisVisible(false)}
+          autoComplete='off'
+          required={required}
+          readOnly={readOnly}
         />
         {
           isVisible ? (
             <div className="typeahead-options-modal">
-              <div className="typeahead-modal-container">
-                {options.map((option) => (
-                  <div
+              {options.map((option) => {
+                if (typeof value === 'string' && option.label.toLowerCase().includes(value.toString().toLowerCase())) {
+                  return <div
                     className="typeahead-option"
-                    onClick={(e) => {onChange(exmy)}}
+                    onMouseDown={() => { 
+                      console.log(name, option.value); 
+                      onChange({ target: { name: name, value: option.value } }); 
+                    }}
+                    key={option.value}
                   >
-                    {option}
-                  </div>
-                ))}
-              </div>
+                    {option.label}
+                  </div>;
+                } else {
+                  return <></>;
+                }
+              })}
+              {options.every((option) => !option.label.toLowerCase().includes(value.toString().toLowerCase())) && (
+                <div className="typeahead-no-match-found">No Match Found</div>
+              )}
             </div>
           ) : (
             <></>
@@ -51,12 +66,37 @@ TFTypeahead.propTypes = {
    * useRef variable for the table which can stop the scrolling of the table
    */
   onChange: PropTypes.func,
+  /**
+   * useRef variable for the table which can stop the scrolling of the table
+   */
+  name: PropTypes.string,
+  /**
+   * useRef variable for the table which can stop the scrolling of the table
+   */
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      label: PropTypes.string
+    })
+  ),
+  /**
+   * Variable for making the input field required
+   */
+  required: PropTypes.bool,
+  /**
+   * Variable for making the input field readOnly
+   */
+  readOnly: PropTypes.bool,
 };
 
 TFTypeahead.defaultProps = {
   placeholder: '',
   value: '',
-  onChange: () => {},
+  onChange: () => { },
+  name: '',
+  options: [],
+  required: false,
+  readOnly: false
 
 };
 
