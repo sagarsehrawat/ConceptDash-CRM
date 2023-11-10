79d908e54c1f0889d01308876932b1187df3cd0d
Sagar Sehrawat <sagar02sehrawat@gmail.com>
2023-11-10 21:58:21 +0530

    update rfp api done

--- src/constants/APIS.ts
+++ src/constants/APIS.ts
@@ -1,6 +1,6 @@
 const APIS = {
-    BASE_URL : 'https://crm-api.conceptdash.ca',
-    // BASE_URL : 'http://localhost:8080',
+    // BASE_URL : 'https://crm-api.conceptdash.ca',
+    BASE_URL : 'http://localhost:8080',
     SERVER_STATUS : `/`,
     GET_RFP_COUNT : '/api/get/rfp/count',
     GET_CITIES : '/api/get/list/cities',
@@ -12,7 +12,8 @@ const APIS = {
     UPDATE_RFP_DATE : '/api/update/rfp/date',
     DELETE_RFPS : '/api/delete/rfp/id',
     GET_GOOGLE_DRIVE_URL : '/google-api/get/fileLink',
-    ADD_RFP : '/api/add/rfp'
+    ADD_RFP : '/api/add/rfp',
+    UPDATE_RFP : '/api/update/rfp'
 };
 
 export default APIS;
