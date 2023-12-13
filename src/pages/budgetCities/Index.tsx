8a881e7f20c80dff20daf6ed9def4dd477442d6b
Utsav Soni <utsav_12013078@nitkkr.ac.in>
2023-12-13 11:20:24 +0530

    re-done mapView

--- src/pages/budgetCities/Index.tsx
+++ src/pages/budgetCities/Index.tsx
@@ -1,29 +1,50 @@
-import React, { useState } from 'react'
-import Header from './sections/Header/Header';
-import HeaderCards from './sections/Header-Cards/HeaderCards';
-import SearchFilter from './sections/SearchFilter/SearchFilter';
-import Table from './sections/Table/Table';
-import Budgets from '../budgets/Index';
+import React, { useState } from "react";
+import Header from "./sections/Header/Header";
+import HeaderCards from "./sections/Header-Cards/HeaderCards";
+import SearchFilter from "./sections/SearchFilter/SearchFilter";
+import Table from "./sections/Table/Table";
+import Budgets from "../budgets/Index";
+import MapView from "./mapView/MapView";
 
 type Props = {
   isCollapsed: boolean;
-}
+};
 const Index = (props: Props) => {
-  const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>("");
+  const [expand, setExpand] = useState<boolean>(false);
   const [cityId, setCityId] = useState<number | null>(null);
 
-  
   return cityId === null ? (
     <>
-      <Header />
-      <HeaderCards />
-      <SearchFilter value={value} setValue={setValue}/>
-      <Table search={value} isCollapsed={props.isCollapsed} setCityId={setCityId} />
+      {expand ? (
+        <MapView expand={expand} setExpand={setExpand} />
+      ) : (
+        <>
+          <div className="d-flex justify-content-between align-items-center">
+            <div>
+              <Header />
+              <HeaderCards />
+              <SearchFilter value={value} setValue={setValue} />
+            </div>
+            <div className="">
+              <MapView expand={expand} setExpand={setExpand} />
+            </div>
+          </div>
+          <Table
+            search={value}
+            isCollapsed={props.isCollapsed}
+            setCityId={setCityId}
+          />
+        </>
+      )}
     </>
-  )
-  : (
-    <Budgets cityId={cityId} setCityId={setCityId} isCollapsed={props.isCollapsed}/>
-  )
-}
+  ) : (
+    <Budgets
+      cityId={cityId}
+      setCityId={setCityId}
+      isCollapsed={props.isCollapsed}
+    />
+  );
+};
 
-export default Index
+export default Index;
