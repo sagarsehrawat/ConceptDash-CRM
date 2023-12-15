6d13db555b9cc5a39ae3d279f41a8efce84f2ea3
anchaltyagiii <anchal@conceptdash.ca>
2023-12-15 13:08:23 +0530

    rfp header cards UI design completed

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,15 @@
 import add_rfp_icon from "../icons/add_rfp_icon.svg";
 import expand_icon from "../icons/expand_icon.svg";
+import total_rfp_icon from "../icons/total_rfp_icon.svg";
+import new_rfp_icon from "../icons/new_rfp_icon.svg";
+import trending_rfp_icon from "../icons/trending_rfp_icon.svg";
+import trending_up_icon from "../icons/trending_up_icon.svg";
 
 export const icons = {
   add_rfp_icon: add_rfp_icon,
   expand_icon: expand_icon,
+  total_rfp_icon: total_rfp_icon,
+  new_rfp_icon: new_rfp_icon,
+  trending_rfp_icon: trending_rfp_icon,
+  trending_up_icon: trending_up_icon,
 };
--- src/pages/rfps/sections/HeaderCards/HeaderCards.tsx
+++ src/pages/rfps/sections/HeaderCards/HeaderCards.tsx
@@ -1,34 +1,64 @@
-import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { initData, selectNewRFPs, selectPercentage, selectTotalRFPs } from '../../../../redux/slices/rfpSlice'
-import SERVICES from '../../../../services/Services'
-import './HeaderCards.css'
-
+// import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  initData,
+  selectNewRFPs,
+  selectPercentage,
+  selectTotalRFPs,
+} from "../../../../redux/slices/rfpSlice";
+import SERVICES from "../../../../services/Services";
+import "./HeaderCards.css";
+import { icons } from "../../../../assets/icons";
+import "./HeaderCards.scss";
 
 const HeaderCards = () => {
   const dispatch = useDispatch();
-  const newRfps : number | string = useSelector(selectNewRFPs);
-  const percent : number | string = useSelector(selectPercentage);
-  const totalRfps : number | string = useSelector(selectTotalRFPs);
+  const newRfps: number | string = useSelector(selectNewRFPs);
+  const percent: number | string = useSelector(selectPercentage);
+  const totalRfps: number | string = useSelector(selectTotalRFPs);
+
+  const { total_rfp_icon, new_rfp_icon, trending_rfp_icon, trending_up_icon } =
+    icons;
 
   useEffect(() => {
     const fetchData = async () => {
-      try{
+      try {
         const response = await SERVICES.rfpCount();
         dispatch(initData(response.res[0]));
-      } catch(error) {
-        console.log(error)
+      } catch (error) {
+        console.log(error);
       }
-    }
+    };
     fetchData();
-  }, [])
-  
+  }, []);
+
+  const cardData = [
+    {
+      icon: total_rfp_icon,
+      rfp: totalRfps,
+      percent: percent,
+      heading: "Total RFP's",
+    },
+    {
+      icon: new_rfp_icon,
+      rfp: newRfps,
+      percent: percent,
+      heading: "New RFP's",
+    },
+    {
+      icon: trending_rfp_icon,
+      rfp: newRfps,
+      percent: percent,
+      heading: "Trending RFP's",
+    },
+  ];
+
   return (
     <>
-      <div className='d-flex flex-row' style={{ marginLeft: "32px", marginBottom: "20px" }}>
-        <div className='header-card'>
+      <div className="rfp-cards-wrapper">
+        {/* <div className='header-card'>
           <p className='header-card-heading'>New RFPs</p>
           <div className=''>
             <p className='header-card-subheading'>{newRfps}</p>
@@ -47,12 +77,33 @@ const HeaderCards = () => {
         <div className='header-card'>
           <p className='header-card-heading'>Total RFPs</p>
           <p className='header-card-subheading'>{totalRfps}</p>
-        </div>
+        </div> */}
+        {cardData.map((item, key) => {
+          return (
+            <div key={key} className="rfp-card-container">
+              <div className="rfp-data-wrapper">
+                <div>
+                  <img src={item.icon} />
+                </div>
+                <div>
+                  <p>{item.heading}</p>
+                  <h3>{item.rfp}</h3>
+                </div>
+              </div>
+              <div className="rfp-percent-wrapper">
+                <div className="gap-2">
+                  <img src={trending_up_icon} /> <span>{percent}%</span>
+                </div>
+                <p>Last Month</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <div className='header-line'></div>
+      <div className="header-line"></div>
       {/* <p className='heading-2' style={{marginLeft : "32px"}}>RFPs</p> */}
     </>
-  )
-}
+  );
+};
 
-export default HeaderCards
+export default HeaderCards;
