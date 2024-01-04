8f4dc918c3f0fc41867530f87fbe5af047444b5e
doma-byte <singhdikshit69@gmail.com>
2024-01-04 19:11:13 +0530

    Add 2025 as new option in budget

--- src/pages/budgets/forms/AddBudget.tsx
+++ src/pages/budgets/forms/AddBudget.tsx
@@ -253,6 +253,7 @@ const AddBudget = ({ show, onHide, budget = null, setApi = () => { }, cityId=0 }
                     { label: '2022', value: '2022' },
                     { label: '2023', value: '2023' },
                     { label: '2024', value: '2024' },
+                    { label: '2025', value: '2025' },
                   ]}
                 />
               </div>
