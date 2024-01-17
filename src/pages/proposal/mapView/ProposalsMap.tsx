c9a445377a4efee26b30a26879cd1d098551318f
Utsav Soni <utsav_12013078@nitkkr.ac.in>
2024-01-17 22:34:33 +0530

    fix

--- src/pages/proposal/mapView/ProposalsMap.tsx
+++ src/pages/proposal/mapView/ProposalsMap.tsx
@@ -14,7 +14,7 @@ import arrowLeft from "../../../assets/icons/Arrow_Left.svg";
 import rightPurple from "../../../assets/icons/Right_Purple.svg";
 import SERVICES from "../../../services/Services";
 import { useDispatch, useSelector } from "react-redux";
-// import LoadingSpinner from "../../../Main/Loader/Loader";
+import LoadingSpinner from "../../../Main/Loader/Loader";
 import Dropdown from "../../../components/form/DropDown/Dropdown";
 
 type Props = {
@@ -111,7 +111,7 @@ const ProposalsMap = ({ expand, setExpand, api, filter, setFilter }: Props) => {
   ]);
   const [zoom, setZoom] = useState<number>(8);
   const [openRightBar, setOpenRightBar] = useState<boolean>(true);
-  // const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isLoading2, setIsLoading2] = useState<boolean>(false);
   const [cities, setcities] = useState<
     Array<{ value: number | string; label: string }>
@@ -132,8 +132,8 @@ const ProposalsMap = ({ expand, setExpand, api, filter, setFilter }: Props) => {
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       try {
-        // setIsLoading(true);
         const response = await SERVICES.getProposals(
           expand ? 999999 : 50,
           1,
@@ -151,10 +151,10 @@ const ProposalsMap = ({ expand, setExpand, api, filter, setFilter }: Props) => {
             Number(response.res[0].city_coordinates[1]),
           ]);
         }
-        // setIsLoading(false);
       } catch (error) {
-        console.log(error);
+        console.log("here: ", error);
       }
+      setIsLoading(false);
     };
     fetchData();
   }, [api, filter, expand, dispatch]);
@@ -178,241 +178,243 @@ const ProposalsMap = ({ expand, setExpand, api, filter, setFilter }: Props) => {
     fetchData();
   }, []);
 
-  // if (isLoading) {
-  //   return <LoadingSpinner />;
-  // } else {
-  return (
-    <div className={expand ? styles["container-expanded"] : styles.container}>
-      {expand && (
-        <div className={styles.header}>
-          <Dropdown
-            name={"Cities"}
-            value={filter.city}
-            checkbox
-            type={false}
-            search
-            disable={isLoading2}
-            onChange={(val: string | number) => {
-              setFilter({ ...filter, city: val });
-            }}
-            options={cities}
-          />
-          <Dropdown
-            name={"Department"}
-            value={filter.dept}
-            checkbox
-            type={false}
-            search
-            disable={isLoading2}
-            onChange={(val: string | number) => {
-              setFilter({ ...filter, dept: val });
-            }}
-            options={depts}
-          />
-          <Dropdown
-            name={"Project Category"}
-            value={filter.cat}
-            checkbox
-            type={false}
-            search
-            disable={isLoading2}
-            onChange={(val: string | number) => {
-              setFilter({ ...filter, cat: val });
-            }}
-            options={projectCategories}
-          />
-          <Dropdown
-            name={"Manager"}
-            value={filter.manager}
-            checkbox
-            type={false}
-            search
-            disable={isLoading2}
-            onChange={(val: string | number) => {
-              setFilter({ ...filter, manager: val });
-            }}
-            options={employees}
-          />
-        </div>
-      )}
-      {!expand && (
-        <div onClick={() => setExpand(true)} className={styles.expand}>
-          <img src={expandIcon} alt="" />
-        </div>
-      )}
-      <div className={styles["map-container"]}>
-        <MapContainer
-          center={center}
-          zoom={zoom}
-          maxZoom={21}
-          key={Math.random()}
-          zoomControl={false}
-          className={styles["leaflet-container"]}
-        >
-          <>
-            <TileLayer
-              url={`${
-                type === 1
-                  ? "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
-                  : type === 2
-                  ? "http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
-                  : ""
-              }`}
-              subdomains={["mt0", "mt1", "mt2", "mt3"]}
-            />
-            <MarkerClusterGroup
-              iconCreateFunction={createClusterCustomIcon}
-              chunkedLoading
-            >
-              {proposals &&
-                proposals.map((proposal, idx) => {
-                  if (
-                    proposal.city_coordinates &&
-                    proposal.city_coordinates.length === 2
-                  ) {
-                    return (
-                      <Marker
-                        key={idx}
-                        icon={customIcon}
-                        position={[
-                          Number(proposal.city_coordinates[0]),
-                          Number(proposal.city_coordinates[1]),
-                        ]}
-                        eventHandlers={{
-                          click: () => {
-                            setCenter([
-                              Number(proposal.city_coordinates[0]),
-                              Number(proposal.city_coordinates[1]),
-                            ]);
-                            setZoom(14);
-                            setcurrentProposal(proposal);
-                          },
-                        }}
-                      ></Marker>
-                    );
-                  } else {
-                    return <></>;
-                  }
-                })}
-            </MarkerClusterGroup>
-          </>
-        </MapContainer>
-      </div>
-      {expand && (
-        <div className={openRightBar ? styles.rightBar : styles.rightBarClose}>
-          <div className={styles.contents}>
-            <div
-              onClick={() => {
-                setExpand(false);
+  if (isLoading) {
+    return <LoadingSpinner />;
+  } else {
+    return (
+      <div className={expand ? styles["container-expanded"] : styles.container}>
+        {expand && (
+          <div className={styles.header}>
+            <Dropdown
+              name={"Cities"}
+              value={filter.city}
+              checkbox
+              type={false}
+              search
+              disable={isLoading2}
+              onChange={(val: string | number) => {
+                setFilter({ ...filter, city: val });
               }}
-              className={styles.backToTable}
-            >
-              Go to Table View <img src={rightPurple} alt="" />
-            </div>
-            {currentProposal && (
+              options={cities}
+            />
+            <Dropdown
+              name={"Department"}
+              value={filter.dept}
+              checkbox
+              type={false}
+              search
+              disable={isLoading2}
+              onChange={(val: string | number) => {
+                setFilter({ ...filter, dept: val });
+              }}
+              options={depts}
+            />
+            <Dropdown
+              name={"Project Category"}
+              value={filter.cat}
+              checkbox
+              type={false}
+              search
+              disable={isLoading2}
+              onChange={(val: string | number) => {
+                setFilter({ ...filter, cat: val });
+              }}
+              options={projectCategories}
+            />
+            <Dropdown
+              name={"Manager"}
+              value={filter.manager}
+              checkbox
+              type={false}
+              search
+              disable={isLoading2}
+              onChange={(val: string | number) => {
+                setFilter({ ...filter, manager: val });
+              }}
+              options={employees}
+            />
+          </div>
+        )}
+        {!expand && (
+          <div onClick={() => setExpand(true)} className={styles.expand}>
+            <img src={expandIcon} alt="" />
+          </div>
+        )}
+        <div className={styles["map-container"]}>
+          <MapContainer
+            center={center}
+            zoom={zoom}
+            maxZoom={21}
+            key={Math.random()}
+            zoomControl={false}
+            className={styles["leaflet-container"]}
+          >
+            <>
+              <TileLayer
+                url={`${
+                  type === 1
+                    ? "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+                    : type === 2
+                    ? "http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+                    : ""
+                }`}
+                subdomains={["mt0", "mt1", "mt2", "mt3"]}
+              />
+              <MarkerClusterGroup
+                iconCreateFunction={createClusterCustomIcon}
+                chunkedLoading
+              >
+                {proposals &&
+                  proposals.map((proposal, idx) => {
+                    if (
+                      proposal.city_coordinates &&
+                      proposal.city_coordinates.length === 2
+                    ) {
+                      return (
+                        <Marker
+                          key={idx}
+                          icon={customIcon}
+                          position={[
+                            Number(proposal.city_coordinates[0]),
+                            Number(proposal.city_coordinates[1]),
+                          ]}
+                          eventHandlers={{
+                            click: () => {
+                              setCenter([
+                                Number(proposal.city_coordinates[0]),
+                                Number(proposal.city_coordinates[1]),
+                              ]);
+                              setZoom(14);
+                              setcurrentProposal(proposal);
+                            },
+                          }}
+                        ></Marker>
+                      );
+                    } else {
+                      return <></>;
+                    }
+                  })}
+              </MarkerClusterGroup>
+            </>
+          </MapContainer>
+        </div>
+        {expand && (
+          <div
+            className={openRightBar ? styles.rightBar : styles.rightBarClose}
+          >
+            <div className={styles.contents}>
               <div
                 onClick={() => {
-                  setZoom(8);
-                  setCenter([
-                    Number(proposals[0].city_coordinates[0]),
-                    Number(proposals[0].city_coordinates[1]),
-                  ]);
-                  setcurrentProposal(null);
+                  setExpand(false);
                 }}
-                className={styles.back}
+                className={styles.backToTable}
               >
-                <img src={arrowLeft} alt="" /> Back
+                Go to Table View <img src={rightPurple} alt="" />
               </div>
-            )}
-            <div className={styles.title}>
-              {currentProposal
-                ? "Proposal Details:"
-                : `Proposals: ${proposals.length}`}
-            </div>
-            <div className={styles.projectList}>
               {currentProposal && (
-                <div className={styles.proposalDetails}>
-                  <div className={styles.metric}>
-                    <div className={styles.label}>Name:</div>
-                    <div className={styles.value}>
-                      {currentProposal.project_name ?? "Not Available"}
-                    </div>
-                  </div>
-                  <div className={styles.metric}>
-                    <div className={styles.label}>City:</div>
-                    <div className={styles.value}>
-                      {currentProposal.city ?? "Not Available"}
-                    </div>
-                  </div>
-                  <div className={styles.metric}>
-                    <div className={styles.label}>Province:</div>
-                    <div className={styles.value}>
-                      {currentProposal.province ?? "Not Available"}
-                    </div>
-                  </div>
-                  <div className={styles.metric}>
-                    <div className={styles.label}>Project Manager:</div>
-                    <div className={styles.value}>
-                      {currentProposal.project_manager ?? "Not Available"}
-                    </div>
-                  </div>
-                  <div className={styles.metric}>
-                    <div className={styles.label}>Department:</div>
-                    <div className={styles.value}>
-                      {currentProposal.department ?? "Not Available"}
-                    </div>
-                  </div>
-                  <div className={styles.metric}>
-                    <div className={styles.label}>Project Category:</div>
-                    <div className={styles.value}>
-                      {currentProposal.project_category ?? "Not Available"}
-                    </div>
-                  </div>
-                  <div className={styles.metric}>
-                    <div className={styles.label}>Client:</div>
-                    <div className={styles.value}>
-                      {currentProposal.client ?? "Not Available"}
-                    </div>
-                  </div>
-                  <div className={styles.metric}>
-                    <div className={styles.label}>Result:</div>
-                    <div className={styles.value}>
-                      {currentProposal.result ?? "Not Available"}
-                    </div>
-                  </div>
+                <div
+                  onClick={() => {
+                    setZoom(8);
+                    setCenter([
+                      Number(proposals[0].city_coordinates[0]),
+                      Number(proposals[0].city_coordinates[1]),
+                    ]);
+                    setcurrentProposal(null);
+                  }}
+                  className={styles.back}
+                >
+                  <img src={arrowLeft} alt="" /> Back
                 </div>
               )}
-              {!currentProposal &&
-                proposals.map((e) => {
-                  return (
-                    <div
-                      onClick={() => {
-                        setCenter([
-                          Number(e.city_coordinates[0]),
-                          Number(e.city_coordinates[1]),
-                        ]);
-                        setZoom(14);
-                        setcurrentProposal(e);
-                      }}
-                      className={styles.projectListItem}
-                    >
-                      {e.project_name}
+              <div className={styles.title}>
+                {currentProposal
+                  ? "Proposal Details:"
+                  : `Proposals: ${proposals.length}`}
+              </div>
+              <div className={styles.projectList}>
+                {currentProposal && (
+                  <div className={styles.proposalDetails}>
+                    <div className={styles.metric}>
+                      <div className={styles.label}>Name:</div>
+                      <div className={styles.value}>
+                        {currentProposal.project_name ?? "Not Available"}
+                      </div>
                     </div>
-                  );
-                })}
+                    <div className={styles.metric}>
+                      <div className={styles.label}>City:</div>
+                      <div className={styles.value}>
+                        {currentProposal.city ?? "Not Available"}
+                      </div>
+                    </div>
+                    <div className={styles.metric}>
+                      <div className={styles.label}>Province:</div>
+                      <div className={styles.value}>
+                        {currentProposal.province ?? "Not Available"}
+                      </div>
+                    </div>
+                    <div className={styles.metric}>
+                      <div className={styles.label}>Project Manager:</div>
+                      <div className={styles.value}>
+                        {currentProposal.project_manager ?? "Not Available"}
+                      </div>
+                    </div>
+                    <div className={styles.metric}>
+                      <div className={styles.label}>Department:</div>
+                      <div className={styles.value}>
+                        {currentProposal.department ?? "Not Available"}
+                      </div>
+                    </div>
+                    <div className={styles.metric}>
+                      <div className={styles.label}>Project Category:</div>
+                      <div className={styles.value}>
+                        {currentProposal.project_category ?? "Not Available"}
+                      </div>
+                    </div>
+                    <div className={styles.metric}>
+                      <div className={styles.label}>Client:</div>
+                      <div className={styles.value}>
+                        {currentProposal.client ?? "Not Available"}
+                      </div>
+                    </div>
+                    <div className={styles.metric}>
+                      <div className={styles.label}>Result:</div>
+                      <div className={styles.value}>
+                        {currentProposal.result ?? "Not Available"}
+                      </div>
+                    </div>
+                  </div>
+                )}
+                {!currentProposal &&
+                  proposals.map((e) => {
+                    return (
+                      <div
+                        onClick={() => {
+                          setCenter([
+                            Number(e.city_coordinates[0]),
+                            Number(e.city_coordinates[1]),
+                          ]);
+                          setZoom(14);
+                          setcurrentProposal(e);
+                        }}
+                        className={styles.projectListItem}
+                      >
+                        {e.project_name}
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+            <div
+              onClick={() => setOpenRightBar(!openRightBar)}
+              className={styles.open}
+            >
+              <img src={openRightBar ? arrowRight : arrowLeft} alt="" />
             </div>
           </div>
-          <div
-            onClick={() => setOpenRightBar(!openRightBar)}
-            className={styles.open}
-          >
-            <img src={openRightBar ? arrowRight : arrowLeft} alt="" />
-          </div>
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 };
-// };
 
 export default ProposalsMap;
